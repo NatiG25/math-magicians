f42d104c84e575ee470f2683f943f86fd15dd583
NatiG <nati.g5684@gmail.com>
2022-05-28 22:49:18 +0300

    Rename calculate.css to style.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './components/calculator.css';
-import ReactDOM from 'react-dom/client';
+import './style.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root'),
 );
