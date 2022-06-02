161ee77ec1581bc6ae1847383deceef0089fad8d
NatiG <nati.g5684@gmail.com>
2022-06-02 23:47:06 +0300

    Fix linter errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,96 +1,96 @@
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import operate from './logic/operate';
 import Calculator from './components/Calculator';
 import About from './components/About';
 import Header from './components/Header';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
 import App from './App';
 import calculate from './logic/calculate';
 
-describe("Operations", () => {
-  test("add two numbers", () => {
-    expect(operate("25", "4", "+")).toBe("29");
+describe('Operations', () => {
+  test('add two numbers', () => {
+    expect(operate('25', '4', '+')).toBe('29');
   });
 
-  test("Subtract two numbers", () => {
-    expect(operate("25", "4", "-")).toBe("21");
+  test('Subtract two numbers', () => {
+    expect(operate('25', '4', '-')).toBe('21');
   });
 
-  test("Multiply two numbers", () => {
-    expect(operate("25", "4", "x")).toBe("100");
+  test('Multiply two numbers', () => {
+    expect(operate('25', '4', 'x')).toBe('100');
   });
 
-  test("Divide two numbers", () => {
-    expect(operate("25", "4", "÷")).toBe("6.25");
+  test('Divide two numbers', () => {
+    expect(operate('25', '4', '÷')).toBe('6.25');
   });
 });
 
-describe("render react components", () => {
-  it("renders correctly", () => {
+describe('render react components', () => {
+  it('renders correctly', () => {
     const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it("renders correctly", () => {
+  it('renders correctly', () => {
     const tree = renderer.create(<About />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it("renders correctly", () => {
+  it('renders correctly', () => {
     const tree = renderer.create(<Home />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it("renders correctly", () => {
+  it('renders correctly', () => {
     const tree = renderer
       .create(
         <Router>
           <Header />
-        </Router>
+        </Router>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it("renders correctly", () => {
+  it('renders correctly', () => {
     const tree = renderer
       .create(
         <Router>
           <App />
-        </Router>
+        </Router>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
 
-describe("Functions test", () => {
-  test("calculate.js test", () => {
+describe('Functions test', () => {
+  test('calculate.js test', () => {
     const initialItems = {
       total: 0,
       next: null,
       operation: null,
     };
-    const buttonName = "=";
-    if (buttonName === ".") {
+    const buttonName = '=';
+    if (buttonName === '.') {
       expect(calculate(initialItems, buttonName)).toEqual({
         total: 0,
         next: null,
         operation: null,
       });
-    } else if (buttonName === ".") {
+    } else if (buttonName === '.') {
       expect(calculate(initialItems, buttonName)).toEqual({
         total: 0.0,
         next: null,
         operation: null,
       });
-    } else if (buttonName === "1") {
+    } else if (buttonName === '1') {
       expect(calculate(initialItems, buttonName)).toEqual({
         total: null,
-        next: "1",
+        next: '1',
       });
-    } else if (buttonName === "=") {
+    } else if (buttonName === '=') {
       expect(calculate(initialItems, buttonName)).toEqual({});
     }
   });
