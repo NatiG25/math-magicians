a602f442d93ffb8ae4ec08803f1ae3e966df9e77
NatiG <nati.g5684@gmail.com>
2023-07-01 03:10:57 +0300

    Add quotes from famous mathmaticians

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,42 +1,60 @@
-import React from "react";
+import React from 'react';
+import React from '../../public/images/mathematicians/AlbertEinstein';
 
 export default function Quote() {
   const quotes = [
     {
       id: 1,
-      quote: `Mathematics is not about numbers, equations, computations, or
+      img: '../../public/images/mathematicians/AlbertEinstein.jpeg',
+      description: `Mathematics is not about numbers, equations, computations, or
       algorithms: it is about understanding.`,
-      mathematician: "Willam Paul Thurston",
+      mathematician: 'Willam Paul Thurston',
     },
     {
       id: 2,
-      quote: `Pure mathematics is, in its way, the poetry of logical ideas.`,
-      mathematician: "Albert Einstein",
+      img: '../../public/images/mathematicians/AlbertEinstein.jpeg',
+      description:
+        'Pure mathematics is, in its way, the poetry of logical ideas.',
+      mathematician: 'Albert Einstein',
     },
     {
       id: 3,
-      quote: `There are two ways to do great mathematics. The first is to be smarter than everybody else. The second way is to be stupider than everybody else but persistent.`,
-      mathematician: "Raoul Bott",
+      img: '../../public/images/mathematicians/AlbertEinstein.jpeg',
+      description:
+        'There are two ways to do great mathematics. The first is to be smarter than everybody else. The second way is to be stupider than everybody else but persistent.',
+      mathematician: 'Raoul Bott',
     },
     {
       id: 4,
-      quote: `Mathematics is not a careful march down a well-cleared highway, but a journey into a strange wilderness, where the explorers often get lost.`,
-      mathematician: "Ronald Graham",
+      img: '../../public/images/mathematicians/AlbertEinstein.jpeg',
+      description:
+        'Mathematics is not a careful march down a well-cleared highway, but a journey into a strange wilderness, where the explorers often get lost.',
+      mathematician: 'Ronald Graham',
     },
     {
       id: 5,
-      quote: `The study of mathematics, like the Nile, begins in minuteness but ends in magnificence.`,
-      mathematician: "Charles Caleb Colton",
+      img: '../../public/images/mathematicians/AlbertEinstein.jpeg',
+      description:
+        'The study of mathematics, like the Nile, begins in minuteness but ends in magnificence.',
+      mathematician: 'Charles Caleb Colton',
     },
     {
       id: 6,
-      quote: `Mathematics is the queen of sciences and arithmetic is the queen of mathematics.`,
-      mathematician: "Carl Friedrich Gauss",
+      img: '../../public/images/mathematicians/AlbertEinstein.jpeg',
+      description:
+        'Mathematics is the queen of sciences and arithmetic is the queen of mathematics.',
+      mathematician: 'Carl Friedrich Gauss',
     },
   ];
   return (
-    <main>
-      <p className="quote">--</p>
-    </main>
+    <ul className="quote-container">
+      {quotes.map((quote) => (
+        <li key={quote.id}>
+          <img src={quote.img} className="quote-img" alt="Mathematician" />
+          <h3 className="quote-title">{quote.mathematician}</h3>
+          <p className="quote-description">{quote.description}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
