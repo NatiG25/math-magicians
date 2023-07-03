341d1151013c554d26867712bbd321e749a918c3
NatiG <nati.g5684@gmail.com>
2023-07-03 22:46:56 +0300

    Download & implement swiper

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,59 +1,117 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+
+import { EffectCoverflow, Pagination } from 'swiper';
+
+// Import images
+import cube from '../images/rubics.png';
+// import next from '../images/next.png';
+// import prev from '../images/prev.png';
+import albert from '../images/mathematicians/AlbertEinstein.jpeg';
 
 export default function Quote() {
   const quotes = [
     {
       id: 1,
       img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSJKyrj1M_E4zNvs1V3mfNkv13MajxmmotwGgNlb639NJVMdPgVDHv29wgH_SnTO_Ep_SPNM6vm_yslNWc', // TODO fix the img src link
-      description: `Mathematics is not about numbers, equations, computations, or
-      algorithms: it is about understanding.`,
-      mathematician: 'Willam Paul Thurston',
+      description: `MATHEMATICS IS NOT ABOUT NUMBERS, EQUATIONS, COMPUTATIONS, OR
+      ALGORITHMS: IT IS ABOUT UNDERSTANDING.`,
+      mathematician: 'WILLIAM PAUL THURSTON',
     },
     {
       id: 2,
-      img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSJKyrj1M_E4zNvs1V3mfNkv13MajxmmotwGgNlb639NJVMdPgVDHv29wgH_SnTO_Ep_SPNM6vm_yslNWc',
+      img: albert,
       description:
-        'Pure mathematics is, in its way, the poetry of logical ideas.',
-      mathematician: 'Albert Einstein',
+        'PURE MATHEMATICS IS, IN ITS WAY, THE POETRY OF LOGICAL IDEAS.',
+      mathematician: 'ALBERT EINSTEIN',
     },
     {
       id: 3,
       img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSJKyrj1M_E4zNvs1V3mfNkv13MajxmmotwGgNlb639NJVMdPgVDHv29wgH_SnTO_Ep_SPNM6vm_yslNWc',
       description:
-        'There are two ways to do great mathematics. The first is to be smarter than everybody else. The second way is to be stupider than everybody else but persistent.',
-      mathematician: 'Raoul Bott',
+        'THERE ARE TWO WAYS TO DO GREAT MATHEMATICS. THE FIRST IS TO BE SMARTER THAN EVERYBODY ELSE. THE SECOND WAY IS TO BE STUPIDER THAN EVERYBODY ELSE BUT PERSISTENT.',
+      mathematician: 'RAOUL BOTT',
     },
     {
       id: 4,
       img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSJKyrj1M_E4zNvs1V3mfNkv13MajxmmotwGgNlb639NJVMdPgVDHv29wgH_SnTO_Ep_SPNM6vm_yslNWc',
       description:
-        'Mathematics is not a careful march down a well-cleared highway, but a journey into a strange wilderness, where the explorers often get lost.',
-      mathematician: 'Ronald Graham',
+        'MATHEMATICS IS NOT A CAREFUL MARCH DOWN A WELL-CLEARED HIGHWAY, BUT A JOURNEY INTO A STRANGE WILDERNESS, WHERE THE EXPLORERS OFTEN GET LOST.',
+      mathematician: 'RONALD GRAHAM',
     },
     {
       id: 5,
       img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSJKyrj1M_E4zNvs1V3mfNkv13MajxmmotwGgNlb639NJVMdPgVDHv29wgH_SnTO_Ep_SPNM6vm_yslNWc',
       description:
-        'The study of mathematics, like the Nile, begins in minuteness but ends in magnificence.',
-      mathematician: 'Charles Caleb Colton',
+        'THE STUDY OF MATHEMATICS, LIKE THE NILE, BEGINS IN MINUTENESS BUT ENDS IN MAGNIFICENCE.',
+      mathematician: 'CHARLES CALEB COLTON',
     },
     {
       id: 6,
       img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSJKyrj1M_E4zNvs1V3mfNkv13MajxmmotwGgNlb639NJVMdPgVDHv29wgH_SnTO_Ep_SPNM6vm_yslNWc',
       description:
-        'Mathematics is the queen of sciences and arithmetic is the queen of mathematics.',
-      mathematician: 'Carl Friedrich Gauss',
+        'MATHEMATICS IS THE QUEEN OF SCIENCES AND ARITHMETIC IS THE QUEEN OF MATHEMATICS.',
+      mathematician: 'CARL FRIEDRICH GAUSS',
     },
   ];
   return (
-    <ul className="quote-container">
-      {quotes.map((quote) => (
-        <li key={quote.id} className="quote-item">
-          <img src={quote.img} className="quote-img" alt="Mathematician" />
-          <h3 className="quote-title">{quote.mathematician}</h3>
-          <p className="quote-description">{quote.description}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <Swiper
+        effect="coverflow"
+        // cssMode={true}
+        grabCursor
+        centeredSlides
+        // loop
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        // pagination={{ el: '.swiper-pagination', clickable: true }}
+        pagination
+        // navigation={{
+        //   nextEl: '.swiper-btn-next',
+        //   prevEl: '.swiper-btn-prev',
+        //   clickable: true,
+        // }}
+        modules={[EffectCoverflow, Pagination]}
+        className="swiper"
+      >
+        <ul className="quote-container">
+          {quotes.map((quote) => (
+            <SwiperSlide key={quote.id} className="swiper-slide">
+              <li className="quote-item">
+                <img
+                  src={quote.img}
+                  className="quote-img"
+                  alt="Mathematician"
+                />
+                <h3 className="quote-title">{quote.mathematician}</h3>
+                <p className="quote-description">{quote.description}</p>
+                <img src={cube} alt="cube" className="quote-img" />
+              </li>
+            </SwiperSlide>
+          ))}
+        </ul>
+        {/* <div className="slider-controller">
+          <div className="swiper-btn-prev slider-arrow">
+            <img src={prev} alt="prev btn" />
+          </div>
+          <div className="swiper-pagination" />
+          <div className="swiper-btn-next slider-arrow">
+            <img src={next} alt="next btn" />
+          </div>
+        </div> */}
+      </Swiper>
+    </>
   );
 }
